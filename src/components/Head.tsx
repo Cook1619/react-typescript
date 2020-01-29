480d9daddc665e303a90a96c647b7259ae4e1665
Cook1619 <cook6348@gmail.com>
2020-01-28 19:39:20 -0600

    Showing the different types od prop types to force how your data is being passed in

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
-//Title
-//isActive
+type User = {
+  name: string;
+};
+
 type Props = {
   title: string;
   isActive?: boolean;
+  thing: number;
+  thing2: string[]; //array of strings
+  status: 'loading' | 'loaded'; //this will only accept one of these 2 strings as props for 'status'
+  thing3: {
+    name: string; //can define what the object would accept
+    age: number;
+  };
+  func: () => void;
+  user: User; //as listed above
 };
 
 export const Head = ({ title = 'Hello', isActive = true }: Props) => {
